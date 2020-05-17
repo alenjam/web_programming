f34519a8266f4a5cf8dbd12d77fae4d491ab932f
Alen James <alenjam@gmail.com>
2020-05-17 19:06:58 +0530

    More updates

---
+++ project0/application.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template # we are now importing just more than Flask!
+
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    names_of_instructors = ["Elie", "Tim", "Matt"]
+    random_name = "Tom"
+    return render_template('index.html', names=names_of_instructors, name=random_name)
+
+@app.route('/second')
+def second():
+    return "WELCOME TO THE SECOND PAGE!"
