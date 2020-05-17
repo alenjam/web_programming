1add1eda0f318771ebebe37b93f4489edc08e76b
Alen James <alenjam@gmail.com>
2020-05-17 18:33:58 +0530

    Adding variables

--- project0/application.py
+++ project0/application.py
@@ -9,3 +9,12 @@ def hello():
 @app.route('/name')
 def welcome():
     return "Welcome to our application!"
+
+@app.route('/welcome/<person>')
+def welcome_home(person):
+    return f"Dear {person}, Welcome Home!!!"
+
+@app.route('/welcome/<int:number>')
+def number_add(number):
+    return f"I guess your favorite number is {number}. \
+                It is half of {number * 2}"
